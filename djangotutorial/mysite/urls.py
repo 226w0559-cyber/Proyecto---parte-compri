1252442d9ddb36d647d98745eb2a3452ca727afe
Rafael Castro <tu_correo@ejemplo.com>
2026-06-09 19:34:16 -0600

    intento1

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView  # Agrega esta importación
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls),
-]
+    path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
+    path('', RedirectView.as_view(url='polls/rafael1/', permanent=True)), # Redirige la raíz a rafael1
