2dd069b089819d7132b7a0656599f09ea8126f42
Rafael Castro <tu_correo@ejemplo.com>
2026-06-09 19:37:43 -0600

    ok

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView  # Agrega esta importación
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
